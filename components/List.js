8b9543b18d529d3d12d415ee4dd9308a562ed3e3
Ilias Doukas <ilias.doukas@gmail.com>
2019-08-31 12:25:01 +0300

    bvalmis

--- components/List.js
+++ components/List.js
@@ -1,32 +1,47 @@
 import React from 'react';
-import {useContext, useState, useEffect} from 'react';
+import {useContext, useEffect} from 'react';
 // eslint-disable-next-line max-len
-import {FlatList, Text} from 'react-native';
+import {FlatList} from 'react-native';
 import PropTypes from 'prop-types';
 import ListItem from './ListItem';
 import {MediaContext} from '../contexts/MediaContext';
-import Data from './Data';
 
 
 const List = () => {
   const [media, setMedia] = useContext(MediaContext);
 
   const getData = () => {
-    fetch('https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((result) => {
-      setMedia(result);
-    });
+    const picLink = 'http://media.mw.metropolia.fi/wbma/uploads/';
+    const dataUrl = 'http://media.mw.metropolia.fi/wbma/media';
+    const idUrl = 'http://media.mw.metropolia.fi/wbma/media/';
+    const mediaArray = [];
+    fetch(dataUrl)
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          result.forEach((n) => {
+            const id = n.file_id;
+            fetch(idUrl + id)
+                .then((response) => {
+                  return response.json();
+                })
+                .then((result) => {
+                  result.thumbnails.w160 = picLink + result.thumbnails.w160;
+                  console.log(result.thumbnails.w160);
+                  mediaArray.push(result);
+                  setMedia(mediaArray);
+                });
+          });
+        });
   };
   useEffect(() => getData(), []);
 
   return (
-      <FlatList
-        data={media}
-        renderItem={({item}) => <ListItem singleMedia={item} />}
-      />
+    <FlatList
+      data={media}
+      renderItem={({item}) => <ListItem singleMedia={item} />}
+    />
   );
 };
 

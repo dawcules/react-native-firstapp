a89e8417f19703cdeaa25fc84ddc536ba1ba7431
Ilias Doukas <ilias.doukas@gmail.com>
2019-09-01 12:32:33 +0300

    stailattu 1

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
 import List from './components/List';
 import {MediaProvider} from './contexts/MediaContext';
 
 
 const App = () => {
   return (
-    <MediaProvider>
-      <View>
+    <MediaProvider style={{flex: 6}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightyellow'}}>
+        <Text style ={{fontWeight: 700, fontSize: 40}}>Newsfeed</Text>
+      </View>
+      <View style={{position: 'absolute', left: 5, top: 31}}>
+        <Image style={{height: 45, width: 45}} source={{uri: 'https://cdn4.iconfinder.com/data/icons/mono-color-web-mobile/250/Home-512.png'}} />
+      </View>
+      <View style={{flex: 5}}>
         <List />
       </View>
     </MediaProvider>
--- components/ListItem.js
+++ components/ListItem.js
@@ -6,13 +6,13 @@ import PropTypes from 'prop-types';
 const ListItem = (props) => {
   return (
     <View style ={{marginTop: 10}}>
-      <TouchableOpacity style={{flex: 2, flexWrap: 'wrap', flexDirection: 'row', padding: 10, marginBottom: 10, backgroundColor: 'lightgray'}}>
-        <Image style={{flex: 1, padding: 5, marginRight: 15}}
+      <TouchableOpacity style={{flex: 2, flexWrap: 'wrap', flexDirection: 'row', padding: 10, backgroundColor: 'lightgray', borderRadius: 20}}>
+        <Image style={{flex: 1, padding: 5, marginRight: 15, borderRadius: 50, height: 100, width: 100}}
           source={{uri: props.singleMedia.thumbnails.w160}}
         />
         <View style = {{flex: 1}}>
-          <Text style={{fontWeight: '800', fontSize: 35}}>{props.singleMedia.title}</Text>
-          <Text>{props.singleMedia.description}</Text>
+          <Text numberOfLines={1} style={{fontWeight: '800', fontSize: 20}}>{props.singleMedia.title}</Text>
+          <Text numberOfLines={5}> {props.singleMedia.description}</Text>
         </View>
       </TouchableOpacity>
     </View>
